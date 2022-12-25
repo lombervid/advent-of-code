79d888d1b58586fc15a2d747e153ba7a9f17b630
David Lombera <david.lomberlan@gmail.com>
2022-12-25 01:05:41 -0600

    Add AoC 2022/02 Part 2

--- 2022/02/main.go
+++ 2022/02/main.go
@@ -14,30 +14,61 @@ func main() {
 	inputFile := utils.GetInputFilePath(defaults.Year, day)
 
 	fileinfo := utils.ReadFile(inputFile)
+	options := getPlayersSelections(fileinfo)
 
-	score := getTotalScorePart1(fileinfo)
+	score := getTotalScorePart1(options)
 	fmt.Println("Part 1:", score)
+
+	score = getTotalScore(options)
+	fmt.Println("Part 2:", score)
 }
 
-func getTotalScorePart1(info []string) int {
+func getPlayersSelections(info []string) [][]string {
+	options := make([][]string, 0)
+
+	for _, value := range info {
+		options = append(options, strings.Split(value, " "))
+	}
+
+	return options
+}
+
+func getTotalScore(matches [][]string) int {
 	var totalScore int
 
-	shape := map[string]map[string]int{
+	shapeToUse := map[string]map[string]string{
+		"A": {"X": "C", "Y": "A", "Z": "B"},
+		"B": {"X": "A", "Y": "B", "Z": "C"},
+		"C": {"X": "B", "Y": "C", "Z": "A"},
+	}
+
+	rewards := map[string]map[string]int{
+		"A": {"A": 3, "B": 0, "C": 6},
+		"B": {"A": 6, "B": 3, "C": 0},
+		"C": {"A": 0, "B": 6, "C": 3},
+	}
+
+	for _, match := range matches {
+		mySelection := shapeToUse[match[0]][match[1]]
+		totalScore += int([]rune(mySelection)[0] - 64) // A => 65; B => 66; C => 67
+		totalScore += rewards[mySelection][match[0]]
+	}
+
+	return totalScore
+}
+
+func getTotalScorePart1(matches [][]string) int {
+	var totalScore int
+
+	rewards := map[string]map[string]int{
 		"X": {"A": 3, "B": 0, "C": 6},
 		"Y": {"A": 6, "B": 3, "C": 0},
 		"Z": {"A": 0, "B": 6, "C": 3},
 	}
 
-	for _, value := range info {
-		scores := strings.Split(value, " ")
-		switch scores[1] {
-		case "X":
-			totalScore += 1 + shape[scores[1]][scores[0]]
-		case "Y":
-			totalScore += 2 + shape[scores[1]][scores[0]]
-		case "Z":
-			totalScore += 3 + shape[scores[1]][scores[0]]
-		}
+	for _, option := range matches {
+		totalScore += int([]rune(option[1])[0] - 87) // X => 88; Y => 89; Z => 90
+		totalScore += rewards[option[1]][option[0]]
 	}
 
 	return totalScore
